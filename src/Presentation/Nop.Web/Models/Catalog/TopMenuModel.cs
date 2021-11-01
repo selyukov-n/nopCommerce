8ed3d8127b3ea663eb24454e5442bb8ae12faef6
Nickolay Selyukov <nselyukov@gmail.com>
2021-11-01 16:28:59 +0500

    support hierarchical topics

--- src/Presentation/Nop.Web/Models/Catalog/TopMenuModel.cs
+++ src/Presentation/Nop.Web/Models/Catalog/TopMenuModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Nop.Web.Framework.Models;
 
@@ -39,6 +40,31 @@ namespace Nop.Web.Models.Catalog
                        && !(DisplayForumsMenuItem && ForumEnabled)
                        && !DisplayContactUsMenuItem;
 
+        public IEnumerable<TopicNodeModel> GetTopicHierarchy()
+        {
+            //var topics = new[]
+            //{
+            //    "Foo // Bar",
+            //    "Foo // Baz",
+            //    "Bar",
+            //    "Bar // Foo // Bar // Baz",
+            //    "Bar // Foo // Bar // Foo",
+            //    "Bar // Foo // Baz // Bar",
+            //}.Select(t => new TopicModel
+            //{
+            //    Id = Topics[0].Id,
+            //    Name = t,
+            //    SeName = Topics[0].SeName,
+            //});
+
+            var root = new TopicTreeBuilder("root");
+            foreach (var t in Topics)
+                root.Add(t);
+
+            var node = root.Build();
+            return node.Children;
+        }
+
         #region Nested classes
 
         public record TopicModel : BaseNopEntityModel
@@ -52,6 +78,53 @@ namespace Nop.Web.Models.Catalog
             public int Level { get; set; }
             public bool ResponsiveMobileMenu { get; set; }
             public CategorySimpleModel Category { get; set; }
+        }
+
+        public record TopicNodeModel
+        {
+            public string Name { get; set; }
+            public TopicModel Topic { get; set; }
+            public ICollection<TopicNodeModel> Children { get; set; } = Array.Empty<TopicNodeModel>();
+        }
+
+        private class TopicTreeBuilder
+        {
+            private readonly IDictionary<string, TopicTreeBuilder> _children = new Dictionary<string, TopicTreeBuilder>(StringComparer.CurrentCultureIgnoreCase);
+            private readonly string _name;
+            private TopicModel _model;
+
+            public TopicTreeBuilder(string name)
+            {
+                _name = name;
+            }
+
+            public void Add(TopicModel model, string[] localPath = null)
+            {
+                localPath ??= model.Name.Split("//")
+                    .Select(s => s.Trim())
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToArray();
+
+                if (localPath.Length == 0)
+                {
+                    _model = model;
+                }
+                else
+                {
+                    var first = localPath[0];
+                    if (!_children.TryGetValue(first, out var ch))
+                        _children[first] = ch = new TopicTreeBuilder(first);
+
+                    ch.Add(model, localPath.Skip(1).ToArray());
+                }
+            }
+
+            public TopicNodeModel Build() => new TopicNodeModel
+            {
+                Name = _name,
+                Topic = _model,
+                Children = _children.Select(c => c.Value.Build()).ToList(),
+            };
         }
 
         #endregion
